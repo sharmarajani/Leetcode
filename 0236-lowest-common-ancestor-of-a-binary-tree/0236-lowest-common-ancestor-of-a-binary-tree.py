daca70476a6a5ee12de513daad6021349458e506
SharmaShriya <31657179+sharmarajani@users.noreply.github.com>
2026-02-26 15:38:03 -0800

    Time: 61 ms (47.58%), Space: 24.5 MB (44.7%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,35 +7,22 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root:
+        if root is None or root==p or root==q:
+            return root
+        left= self.lowestCommonAncestor(root.left,p,q)
+        right= self.lowestCommonAncestor(root.right,p,q)
+        if left!=None and right!=None:
+            return root
+        elif left!=None:
+            return left
+        elif right!=None:
+            return right
+        else:
             return None
-        path1 = []
-        path2 = []
-        def bactrack (root, target , path):
-            # basecase
-            
-            if not root:
-                return False
-            # logic
+        
 
-            path.append(root)
-            if root==target:
 
-                return True
 
-            if bactrack(root.left, target, path) or bactrack(root.right, target, path):
-                return True
-            # backtrack
-            path.pop()
-        # return False
-        bactrack(root, p, path1)
-        bactrack(root, q, path2)
-        i=0
-        while i<len(path1) and i <len(path2):
-            if path1[i]==path2[i]:
-                i+=1
-            else:
-                return path1[i-1]
-        return path1[i-1]
 
+        
         
