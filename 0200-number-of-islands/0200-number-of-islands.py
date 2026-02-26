d17be43b6381ef2d75a6ae2078ea07f4d509b5f7
SharmaShriya <31657179+sharmarajani@users.noreply.github.com>
2026-02-25 16:07:35 -0800

    Time: 246 ms (54.18%), Space: 21.7 MB (47.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,31 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if grid==None or len(grid)==0:
-            return None
-        q = collections.deque()
+        dirs= [[0,1], [0,-1], [-1,0], [1,0]]
         m= len(grid)
-        n=len(grid[0])
-        size=0
-        dirs = [[-1,0],[1,0],[0,1],[0,-1]]
+        n = len(grid[0])
+        count = 0
+
+        def dfs ( nr, nc):
+            if nr < 0 or nr >= m or nc < 0 or nc >= n or grid[nr][nc] == "0" :
+                return 
+            grid[nr][nc] = "0"
+            for diri in dirs:
+                r = diri[0] + nr
+                c = diri[1] + nc
+                dfs(r,c)
+            
+
         for i in range(m):
             for j in range(n):
-                if grid[i][j]=="1":
-                    size+=1
-                    grid[i][j]=0
-                    q.append([i,j])
-                    while q:
-                        row,col = q.popleft()
-                        for diri in dirs:
-                            nr = row + diri[0]
-                            nc = col + diri[1]
-                            if 0 <= nr < m and 0<=nc < n:
-                                if grid[nr][nc]=="1":
-                                    q.append([nr,nc])
-                                    grid[nr][nc] = '0'
-        return size
+                if grid[i][j]== "1":
+                    count+=1
+                    dfs(i,j)
+        return count
 
-                    
-
-
-         
         
