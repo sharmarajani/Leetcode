f22f321d30a31716562a5cbc7fe35395df6bf139
SharmaShriya <31657179+sharmarajani@users.noreply.github.com>
2026-03-05 18:59:36 -0800

    Time: 8 ms (8.05%), Space: 19.5 MB (75%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,35 +1,32 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        m = len(nums1)
-        n = len(nums2)
-        res = []
-        i , j = 0,0
-
-        while i < m and j < n:
-            if nums1[i] <= nums2[j]:
-                res.append(nums1[i])
-                i += 1
+        n1, n2 = len(nums1) , len(nums2)
+        n = (n1+n2)//2
+        if n1 > n2:
+            return self.findMedianSortedArrays(nums2, nums1)
+        
+        lo, hi = 0, n1
+        left = (n1+n2+1)//2
+        while lo <= hi:
+            mid1 = (lo + hi) //2
+            mid2 = left - mid1
+            l1, l2, r1, r2 = float("-inf"), float("-inf"), float("inf"), float("inf")
+            if mid1 < n1:
+                r1=nums1[mid1]
+            if mid2 < n2:
+                r2= nums2[mid2]
+            if mid1-1 >=0:
+                l1 = nums1[mid1-1]
+            if mid2-1>=0:
+                l2 = nums2[mid2-1]
+            if l1<=r2 and l2<=r1:
+                total = n1+n2
+                if total %2==1:
+                    return float(max(l1, l2))
+                else:
+                    return (max(l1, l2) + min(r1, r2)) / 2.0
+            elif l1> r2:
+                hi = mid1-1
             else:
-                res.append(nums2[j])
-                j += 1
-
-        while i < m:
-            res.append(nums1[i])
-            i += 1
-
-        while j < n:
-            res.append(nums2[j])
-            j += 1
-
-        if len(res) % 2 ==1:
-            return res[len(res)//2]
-            
-            
-        else:
-            index = len(res)//2
-            return (res[index] + res[index-1])/2
-            
-            
-
-
-            
+                lo = mid1+1
+        return 0
