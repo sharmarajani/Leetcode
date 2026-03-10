63674e4a6e1e7e24ae4c846c6c2250d46863d9f5
SharmaShriya <31657179+sharmarajani@users.noreply.github.com>
2026-03-10 16:43:38 -0700

    Time: 14 ms (5.9%), Space: 19.8 MB (74.85%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        Bipartite = True
+        color = [-1] * len(graph)
+        def dfs (graph, color, idx):
+            # base
+            nonlocal Bipartite
+            if not Bipartite:
+                return
+            # logic
+            for nei in graph[idx]:
+                if color[nei] == -1:
+                    color[nei] = 1-color[idx]
+                    dfs(graph, color, nei)
+                else:
+                    if color[nei]==color[idx]:
+                        Bipartite = False
+                        return
+        for i in range(len(graph)):
+            if color[i]==-1:
+                dfs(graph, color, i)
+        return Bipartite
+        
