7163b80490096f4c68d3bc2e40f14bd85663c028
SharmaShriya <31657179+sharmarajani@users.noreply.github.com>
2026-03-15 11:09:33 -0700

    Time: 7 ms (46.22%), Space: 20.3 MB (91.05%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,39 +1,34 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        q=deque()
-        hmap=defaultdict(list)
-        inbound =[0] * numCourses
-
-        count=0
+        hmap = defaultdict(list)
+        inbound = [0] * numCourses
+        cnt = 0
+        q= deque()
         for i in prerequisites:
-            inbound[i[0]] +=1
-            # if i[1] not in hmap:
-            #     hmap[i[1]] =[]
             hmap[i[1]].append(i[0])
+            inbound[i[0]]+=1
         
         for i in range(numCourses):
-            if inbound[i]==0:
-                count+=1
+            if inbound[i] ==0:
+                cnt+=1
                 q.append(i)
-            
-        if count ==numCourses:
+        if cnt == numCourses:
             return True
         if not q:
             return False
-
+        
         while q:
-            curr= q.popleft()
-            dependency=hmap[curr]
-            if dependency:
-                for i in dependency:
-                    inbound[i]-=1
-                    if inbound[i]==0:
+            idx = q.popleft()
+            if hmap[idx]:
+                for i in hmap[idx]:
+                    inbound[i] -=1
+                    if inbound[i] == 0:
                         q.append(i)
-                        count+=1
-                    if count == numCourses:
+                        cnt+=1
+                    if cnt == numCourses:
                         return True
         return False
-
-
+                    
+            
 
         
